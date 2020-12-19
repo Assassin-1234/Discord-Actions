d22cab00ab387c4ee836cd7f31924b5ef717399d
TheRamann <69319475+TheRamann@users.noreply.github.com>
2020-12-19 12:37:31 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
 const superagent = require("superagent");
 
 function hug() {
+    async function my(){
     var {
         body
     } = await superagent.get("https://nekos.life/api/v2/img/hug");
     var gif = body.url;
-    return gif;
+    return gif;}
+    my()
 }
 function kiss(){
+    async function my(){
     var {
         body
     } = await superagent.get("https://nekos.life/api/kiss");
     var gif = body.url;
-    return gif;
+    return gif;}
+    my()
 }
 function pat(){
+    async function my(){
     var {
         body
     } = await superagent.get("https://nekos.life/api/v2/img/pat");
     var gif = body.url;
-    return gif;
+    return gif;}
+    my()
 }
 function poke(){
+    async function my(){
     var {
         body
     } = await superagent.get("https://nekos.life/api/v2/img/poke");
     var gif = body.url;
-    return gif;
+    return gif;}
+    my()
 }
 function slap(){
+    async function my(){
     var {
         body
     } = await superagent.get("https://nekos.life/api/v2/img/slap");
     var gif = body.url;
-    return gif;
+    return gif;}
+    my()
 }
 function tickle(){
+    async function my(){
     var {
         body
     } = await superagent.get("https://nekos.life/api/v2/img/tickle");
     var gif = body.url;
-    return gif;
+    return gif;}
+    my()
 }
 function smug(){
+    async function my(){
     var {
         body
     } = await superagent.get("https://nekos.life/api/v2/img/smug");
     var gif = body.url;
-    return gif;
+    return gif;}
+    my()
 }
 
 module.exports.hug = hug;
